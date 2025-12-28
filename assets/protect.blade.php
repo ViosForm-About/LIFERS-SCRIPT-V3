c3c99e7c0ec9c709dbecbf13a9708ac2bbf64aed
Vios About <tyzcelk2@gmail.com>
2025-12-28 11:19:46 +0700

    Create protect.blade.php

---
+++ assets/protect.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Protected</title>
+<style>
+body {
+  background:#f5f7fa;
+  font-family:Arial;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  height:100vh;
+}
+.box {
+  text-align:center;
+}
+</style>
+</head>
+<body>
+<div class="box">
+  <h1>PROTECT LIFERS</h1>
+  <p>by : Lifers</p>
+</div>
+</body>
+</html>
